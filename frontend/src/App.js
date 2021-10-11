cc8ed5ee15c261cd59029590f966fd5c96a25f70
Thuy Le <phuongthuy9796@gmail.com>
2021-10-11 00:02:16 -0700

    Finish buy cake feature using hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 import store from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
