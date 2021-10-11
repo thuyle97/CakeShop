1a032563ba6eabdb08f0de3e43ef96349e3b99a3
Thuy Le <phuongthuy9796@gmail.com>
2021-10-10 23:37:48 -0700

    Finish feature buy cake

--- frontend/src/components/CakeContainer.js
+++ frontend/src/components/CakeContainer.js
@@ -5,7 +5,7 @@ import { buyCake } from "../redux";
 function cakeContainer(props) {
   return (
     <div>
-      <h2> Number of Cakes</h2>
+      <h2> Number of Cakes - {props.numOfCakes}</h2>
       <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   );
