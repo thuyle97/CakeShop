6111814cf3c297a08774abe02c1090eccebe2bd3
Thuy Le <phuongthuy9796@gmail.com>
2021-10-12 14:44:10 -0700

    Add Dispatch Hook

--- frontend/src/components/IceCreamContainer.js
+++ frontend/src/components/IceCreamContainer.js
@@ -5,7 +5,7 @@ import { buyIceCream } from "../redux";
 function IceCreamContainer(props) {
   return (
     <div>
-      <h2>Number of Icecreams - {props.numOfIceCream}</h2>
+      <h2>Number of Icecreams - {props.numOfIceCreams}</h2>
       <button onClick={props.buyIceCream}>Buy Icecream</button>
     </div>
   );
@@ -13,7 +13,7 @@ function IceCreamContainer(props) {
 
 const mapStateToProps = (state) => {
   return {
-    numOfIceCream: state.iceCream.numOfIceCream,
+    numOfIceCreams: state.iceCream.numOfIceCreams,
   };
 };
 
