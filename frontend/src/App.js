cf4242efada77925d45a7d986490f42b696ec237
Thuy Le <phuongthuy9796@gmail.com>
2021-10-12 15:24:32 -0700

    Add action payload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 import store from "./redux/store";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
